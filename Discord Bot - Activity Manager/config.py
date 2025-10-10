825e5fa665deba53ba434f357f2d3b10d80501d0
Brandon Williams <148785986+Paiboy1@users.noreply.github.com>
2025-10-10 12:37:20 +0100

    Update config.py

--- Discord Bot - Activity Manager/config.py
+++ Discord Bot - Activity Manager/config.py
@@ -4,8 +4,11 @@ SPREADSHEET_ID = '1dCYKOhi0x5NjzS9XGAJCY-G7FOi8CBJudTFIE3gsJzc'
 SHEET_NAME = 'Roster'
 FORUM_CHANNEL_ID = 1339048014890795050
 BOT_COMMANDS_CHANNEL_ID = 1332029492130086956
-CREDENTIALS_FILE = 'None' # Removed due to Microsofts security on tokens being uploaded.
+SERVER_ID = 1418592243329273909
 LOA_CHANNEL_ID = 1332029492130086960
+SESSION_STATUS_CHANNEL_ID = 1424715912409255976
+
+CREDENTIALS_FILE = 'None' # Removed due to Microsofts security on tokens being uploaded.
 
 # Google Sheets column index
 POINTS_COLUMN = 15      # Column P
@@ -18,3 +21,6 @@ DISCORD_ID_COLUMN = 16  # Column Q
 # Points system
 POINTS_PER_HOUR = 5
 MIN_HOURS_FOR_POINTS = 1
+
+# MR Ascension Form
+MR_ASCENSION_FORM_URL = 'https://tinyurl.com/MR-Ascension-App'
