7e70af03f07dfe28c946148f57012e3fbc07ff23
Brandon Williams <148785986+Paiboy1@users.noreply.github.com>
2025-12-22 09:45:21 +0000

    Update config.py

--- Discord Bot - Activity Manager/config.py
+++ Discord Bot - Activity Manager/config.py
@@ -1,5 +1,5 @@
 # Settings file for the Discord bot
-DISCORD_TOKEN = 'None' # Removed due to Microsofts security on tokens being uploaded.
+DISCORD_TOKEN = 'None' # Removed due to Discords security on tokens being uploaded.
 SPREADSHEET_ID = '1dCYKOhi0x5NjzS9XGAJCY-G7FOi8CBJudTFIE3gsJzc'
 SHEET_NAME = 'Roster'
 FORUM_CHANNEL_ID = 1339048014890795050
@@ -25,4 +25,5 @@ MIN_HOURS_FOR_POINTS = 1
 
 # MR Ascension Form
 MR_ASCENSION_FORM_URL = 'https://tinyurl.com/MR-Ascension-App'
+
 
